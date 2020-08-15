899d272d6efc2961fe6daf0b815a4dc7294977d6
karantaley <47653220+karantaley@users.noreply.github.com>
2020-08-15 22:16:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-exports.PREFIX = '+';
 
-exports.TOKEN = 'NzQwNTkzMjQzMzUxMjg1ODYw.XyrRNA.6dUysZyTJwXjJZTOsx4i5m_HmKQ';
-
-exports.GOOGLE_API_KEY = 'AIzaSyA1lEz5zZqbR9l0gY8OwToXBz9NlKp56lI';
-
-exports.GENIUS_API_KEY = 'yA8IP41a920sc7iqgSCew5eKTR17O0KxGS6FyGxWyhTz-ZpRpWBb7z0fhYC8yG-D';
