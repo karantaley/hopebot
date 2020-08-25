8b6db14b8cdc1d9baaf1869f3a18057b5d1c18ad
Firez <55920870+FirezzzZ@users.noreply.github.com>
2020-08-25 09:05:17 +0530

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
-module.exports = async bot => {
+module.exports = bot => {
     console.log(`${bot.user.username} is available now!`);
     bot.user.setActivity(`${bot.users.cache.size} users!`, {
         type: "WATCHING"
