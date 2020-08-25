ef4b1f666603086092dd181f9dad14b0bab70cc5
Firez <55920870+FirezzzZ@users.noreply.github.com>
2020-08-25 09:03:49 +0530

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -11,10 +11,11 @@ module.exports = {
     },
     run: async (bot, message, args) => {
         try {
+            let msg = await message.channel.send(`Pinging..`);
             const embed = new MessageEmbed()
                 .setColor("GREEN")
-                .setDescription(`💓 ${Math.round(bot.ws.ping)}`)
-            await message.channel.send({ embed: embed });
+                .setDescription(`:hourglass_flowing_sand: ${msg.createdTimestamp - message.createdTimestamp}\n💓 ${Math.round(bot.ws.ping)}`)
+            await msg.edit(``, { embed: embed })
         } catch (error) {
             return console.error(error);
         };
