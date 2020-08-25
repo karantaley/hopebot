3dd79d3f9f028ad02a140cfdad37a934acf0c04b
Firez <55920870+FirezzzZ@users.noreply.github.com>
2020-08-25 09:13:40 +0530

    Update command.js

--- handler/command.js
+++ handler/command.js
@@ -9,5 +9,5 @@ module.exports = (bot) => {
             if (pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
         };
     };
-    ["info", "music"].forEach(x => load(x));
+    ["info", "music", "moderation"].forEach(x => load(x));
 };
