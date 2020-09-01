7fdec97327606b37d063843643f8103071391a56
Firez <55920870+FirezzzZ@users.noreply.github.com>
2020-09-01 14:03:22 +0530

    Update skip.js

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,4 +1,5 @@
-const { VoiceState } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+let USED = false;
 
 module.exports = {
     config: {
@@ -11,19 +12,81 @@ module.exports = {
     },
     run: async (bot, message, args, ops) => {
         try {
-            const { channel } = message.member.voice;
-            if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to stop music!');
-            if (message.guild.me.voice.channel !== message.member.voice.channel) return message.channel.send("**You Have To Be In The Same Channel With The Bot!**");
+            const { id } = message.guild;
 
-            let role = message.guild.roles.cache.find(r => r.name.toUpperCase() === 'DJ');
-            if (!role) return message.channel.send('**Role Not Found! - DJ**');
-            if (!message.member.roles.cache.has(role.id)) return message.channel.send('**Cannot Skip, Missing Role DJ**!');
-            const serverQueue = ops.queue.get(message.guild.id);
-            if (!serverQueue) return message.channel.send(':x: **Nothing playing in this server**');
-            serverQueue.songs.length === 1 ? serverQueue.connection.dispatcher.end() : serverQueue.connection.dispatcher.end();
-            await message.channel.send(':fast_forward: Skipped');
-            await channel.join();
-            return;
+            const role = message.guild.roles.cache.find(r => r.name.toUpperCase() === 'DJ');
+            if (!role) return message.channel.send('**Role Not Found - DJ**');
+
+            const player = bot.music.players.get(id);
+            if (!player) return message.channel.send('**I Am Not Connected To Any Voice Channel!**');
+
+            if (player.queue.size === 0) return message.channel.send('**Nothing Is Being Played!**');
+
+            const { channel } = message.member.voice;
+            if (!channel && !message.member.roles.cache.has(role.id) && !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('**You Are Not Connected To Any Voice Channel!**');
+
+            if (channel && !message.member.roles.cache.has(role.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                if (player.voiceChannel.id === channel.id) {
+                    const members = channel.members.filter(r => !r.user.bot);
+                    if (members.size === 1) {
+                        player.stop();
+                    } else {
+                        if (!USED) {
+                            USED = true;
+                            const votesRequired = Math.ceil(members.size * .6);
+
+                            const embed = new MessageEmbed()
+                                .setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true }))
+                                .setColor('GREEN')
+                                .setDescription(`**Total Votes Required To Skip! - ${votesRequired}**`)
+                                .setTimestamp();
+                            let msg = await message.channel.send({ embed: embed });
+                            await msg.react('⏩');
+                            await msg.react('❎');
+
+                            const filter = (reaction, user) => {
+                                if (user.bot) return false;
+                                const filterChannel = message.guild.members.cache.get(user.id).voice.channel;
+                                if (filterChannel) {
+                                    if (filterChannel.id === player.voiceChannel.id) {
+                                        return ['⏩'].includes(reaction.emoji.name);
+                                    };
+                                    return false;
+                                } else {
+                                    return false;
+                                };
+                            };
+
+                            try {
+                                const collector = await msg.awaitReactions(filter, {
+                                    max: votesRequired,
+                                    time: 60000,
+                                    errors: ['time']
+                                });
+                                const totalVotes = collector.get('⏩').users.cache.filter(u => !u.bot);
+                                if (totalVotes.size >= votesRequired) {
+                                    player.stop();
+                                    USED = false;
+                                    return message.channel.send(`**Skipped Song ${player.queue[0].title}**`);
+                                } else {
+                                    return message.channel.send(`**Could Not Skip The Song, Not Enough Reactions!**`);
+                                };
+                            } catch (error) {
+                                console.error(error);
+                                USED = false;
+                                return message.channel.send(`An Error Occurred: \`${error.message}\`!`);
+                            }
+                        } else {
+                            return message.channel.send(`**React On The Emoji To Skip!**`);
+                        };
+                    };
+                } else {
+                    return message.channel.send('**Please Join The VC In Which The Bot Is Currently Playing Music!**');
+                };
+            } else if (message.member.roles.cache.has(role.id) || message.member.permissions.has('ADMINISTRATOR')) {
+                player.stop();
+                return message.channel.send(`**Skipped Song ${player.queue[0].title}**`);
+            };
         } catch (error) {
             console.error(error);
             return message.channel.send(`An Error Occurred: \`${error.message}\`!`);
