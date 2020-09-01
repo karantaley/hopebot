2c2696171c7d699a638685a440e79faac8a89df8
Firez <55920870+FirezzzZ@users.noreply.github.com>
2020-09-01 14:06:45 +0530

    Create loopqueue.js

---
+++ commands/music/loopqueue.js
@@ -0,0 +1,47 @@
+module.exports = {
+    config: {
+        name: 'loopqueue',
+        aliases: ["lq"],
+        category: "music",
+        description: 'Repeats All Songs In The Queue',
+        usage: " ",
+        accessableby: "everyone"
+    },
+    run: async (bot, message, args, ops) => {
+        try {
+            const role = message.guild.roles.cache.find(r => r.name.toUpperCase() === 'DJ');
+            if (!role) return message.channel.send('**Role Not Found - DJ**');
+
+            const { channel } = message.member.voice;
+            if (!channel && !message.member.roles.cache.has(role.id) && !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('**You Are Not Connected To Any Voice Channel!**');
+
+            const serverQueue = bot.music.players.get(message.guild.id);
+            if (!serverQueue || serverQueue.queue.size === 0) return message.channel.send('❌ **Nothing playing in this server**');
+
+            if (channel && !message.member.roles.cache.has(role.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                if (serverQueue.voiceChannel.id === channel.id) {
+                    if (!serverQueue.queueRepeat) {
+                        serverQueue.setQueueRepeat(true);
+                        return message.channel.send('**🔁 Queue Has Been Looped!**');
+                    } else {
+                        serverQueue.setQueueRepeat(false);
+                        return message.channel.send('**🔁 Queue Has Been Unlooped!**');
+                    };
+                } else {
+                    return message.channel.send('**Please Join The VC In Which The Bot Is Currently Playing Music!**');
+                };
+            } else if (message.member.roles.cache.has(role.id) || message.member.permissions.has('ADMINISTRATOR')) {
+                if (!serverQueue.queueRepeat) {
+                    serverQueue.setQueueRepeat(true);
+                    return message.channel.send('**🔁 Queue Has Been Looped!**');
+                } else {
+                    serverQueue.setQueueRepeat(false);
+                    return message.channel.send('**🔁 Queue Has Been Unlooped!**');
+                };
+            };
+        } catch (error) {
+            console.error(error);
+            return message.channel.send(`An Error Occurred: \`${error.message}\`!`);
+        };
+    }
+};
