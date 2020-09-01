eb9892b193a9451e44b9365500799f4ee9e2a25a
Firez <55920870+FirezzzZ@users.noreply.github.com>
2020-09-01 13:58:51 +0530

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,7 @@
-module.exports = bot => {
+module.exports = async bot => {
     console.log(`${bot.user.username} is available now!`);
     bot.user.setActivity(`${bot.users.cache.size} users!`, {
         type: "WATCHING"
     });
+    bot.erela.nodeConnect(bot);
 };
