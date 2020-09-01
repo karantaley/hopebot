a2806479dcf2c66dc2be937d7696d8465514993e
Firez <55920870+FirezzzZ@users.noreply.github.com>
2020-09-01 14:02:33 +0530

    Update clear.js

--- commands/music/clear.js
+++ commands/music/clear.js
@@ -9,21 +9,28 @@ module.exports = {
     },
     run: async (bot, message, args, ops) => {
         try {
-            let role = message.guild.roles.cache.find(r => r.name.toUpperCase() === 'DJ');
-            if (!role) return message.channel.send('**Role Not Found! - DJ**');
-            if (!message.member.roles.cache.has(role.id)) return message.channel.send('**Cannot Skip, Missing Role DJ**!');
+            const role = message.guild.roles.cache.find(r => r.name.toUpperCase() === 'DJ');
+            if (!role) return message.channel.send('**Role Not Found - DJ**');
+
             const { channel } = message.member.voice;
-            if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to skip music!');
-            if (message.guild.me.voice.channel !== message.member.voice.channel) {
-                return message.channel.send("**You Have To Be In The Same Channel With The Bot!**");
-            }
-            const serverQueue = ops.queue.get(message.guild.id);
-            if (!serverQueue) return message.channel.send(':x: **Nothing playing in this server**');
-            if (!serverQueue.songs) return message.channel.send(':x: **There are No Songs In The Queue!**')
-            serverQueue.songs = [];
-            serverQueue.connection.dispatcher.end();
-            await message.channel.send("Skipped All Songs");
-            return await channel.join();
+            if (!channel && !message.member.roles.cache.has(role.id) && !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('**You Are Not Connected To Any Voice Channel!**');
+
+            const serverQueue = bot.music.players.get(message.guild.id);
+            if (!serverQueue || serverQueue.queue.size === 0) return message.channel.send('❌ **Nothing Playing In This Server!**');
+
+            if (channel && !message.member.roles.cache.has(role.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                if (serverQueue.voiceChannel.id === channel.id) {
+                    serverQueue.queue.length = [];
+                    return message.channel.send("✅ **Skipped All Songs!**");
+                } else {
+                    return message.channel.send('**Please Join The VC In Which The Bot Is Currently Playing Music!**');
+                };
+            } else if (message.member.roles.cache.has(role.id) || message.member.permissions.has('ADMINISTRATOR')) {
+                serverQueue.stop();
+                serverQueue.queue.length = [];
+                serverQueue.playing = false;
+                return message.channel.send("✅ **Skipped All Songs!**");
+            };
         } catch (error) {
             console.error(error);
             return message.channel.send(`An Error Occurred: \`${error.message}\`!`);
