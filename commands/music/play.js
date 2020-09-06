994e38c7d2eb6cd8dccb5c266627c0b6a301593d
Firez <55920870+FirezzzZ@users.noreply.github.com>
2020-09-06 18:41:46 +0530

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,4 +1,6 @@
 const { MessageEmbed } = require('discord.js');
+const play = require('../../structures/loadTracks');
+const { getData, getPreview } = require('spotify-url-info');
 
 module.exports = {
     config: {
@@ -6,27 +8,13 @@ module.exports = {
         aliases: ['p'],
         category: 'music',
         description: 'Plays Music In Voice Channel',
-        usage: '[song name | song link]',
+        usage: '[yt | bc | sc | mix | twitch] (optional) [song name | song link]\n\nEg:-\n+play youtube Imagine Dragons\n+play soundcloud Anything You Want\n+play Imagine Dragons',
         accessableby: 'everyone'
     },
-    run: async (bot, message, args, ops) => {
+    run: async (bot, message, args) => {
         try {
-            let searchResults;
-            const searchString = args.join(' ');
             const { channel } = message.member.voice;
             if (!channel) return message.channel.send('**Please Join A Voice Channel!**');
-
-            try {
-                searchResults = await bot.music.search(searchString, message.author);
-            } catch {
-                try {
-                    searchResults = await bot.music.search(searchString, message.author);
-                } catch (error) {
-                    return message.channel.send('**❌ No Matches!**');
-                };
-            };
-            const tracks = searchResults.tracks.slice(0, 1);
-            if (tracks.length === 0) return message.channel.send('**❌ No Matches!**');
 
             let player = bot.music.players.get(message.guild.id);
             if (!player) {
@@ -39,18 +27,50 @@ module.exports = {
                 );
                 player = bot.music.players.get(message.guild.id);
             };
-            const track = tracks[0];
-            player.queue.add(track);
 
-            if (!player.playing) return player.play();
-            const sembed = new MessageEmbed()
-                .setColor("GREEN")
-                .setTitle("Added To Queue")
-                .setThumbnail(`https://i.ytimg.com/vi/${track.identifier}/hqdefault.jpg`)
-                .setDescription(`**${track.title}** has been added to queue! | Requested By **${message.author.username}**`)
-                .setFooter(message.member.displayName, message.author.displayAvatarURL({ dynamic: true }))
-                .setTimestamp();
-            return message.channel.send({ embed: sembed });
+            if (args[0].startsWith('https://open.spotify.com')) {
+                const data = await getData(args.join(''));
+                if (data.type === 'playlist' || data.type === 'album') {
+                    let songsToAdd = data.tracks.items.length;
+                    if (data.type == 'playlist') {
+                        for (let i = 0; i < songsToAdd; i++) {
+                            const song = data.tracks.items[i];
+                            play(bot, message, player, `${song.track.name} ${song.track.artists[0].name}`, true);
+                        }
+                    } else {
+                        await data.tracks.items.forEach(song => {
+                            play(bot, message, player, `${song.name} ${song.artists[0].name}`, true);
+                        });
+                    };
+
+                    const playlistInfo = await getPreview(args.join(''));
+                    const embed = new MessageEmbed()
+                        .setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true }))
+                        .setTitle('Playlist Added')
+                        .setColor('GREEN')
+                        .setURL(args.join(''))
+                        .setDescription(`__Playlist__ - **${playlistInfo.title}**\n\nPlaylist Length: **${songsToAdd}**`)
+                        .setFooter(`Requested By ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
+                        .setTimestamp();
+                    return message.channel.send({ embed: embed });
+                } else if (data.type == 'track') {
+                    const track = await getPreview(args.join(' '));
+                    play(bot, message, player, `${track.title} ${track.artist}`, false);
+                };
+            } else {
+                if (['youtube', 'yt', 'sc', 'soundcloud', 'bc', 'bandcamp', 'mix', 'mixer', 'twitch'].includes(args[0].toLowerCase())) {
+                    if (args[0].toLowerCase() === 'yt') args[0] = 'youtube';
+                    if (args[0].toLowerCase() === 'sc') args[0] = 'soundcloud';
+                    if (args[0].toLowerCase() === 'bc') args[0] = 'bandcamp';
+                    if (args[0].toLowerCase() === 'mix') args[0] = 'mixer';
+                    
+                    searchQuery = {
+                        source: args[0],
+                        query: args.slice(1).join(' '),
+                    };
+                } else searchQuery = args.join(' ');
+                play(bot, message, player, searchQuery, false);
+            };
         } catch (error) {
             console.error(error);
             return message.channel.send(`An Error Occurred: \`${error.message}\`!`);
