34a38be635ccf5facd8cd0ee948e9210b14c6757
Firez <55920870+FirezzzZ@users.noreply.github.com>
2020-09-08 15:28:12 +0530

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -13,9 +13,7 @@ module.exports = {
             if (!role) return message.channel.send('**Role Not Found - DJ**');
 
             const player = bot.music.players.get(message.guild.id);
-            if (!player) return message.channel.send('**I Am Not Connected To Any Voice Channel!**');
-
-            if (player.queue.size === 0) return message.channel.send('**Nothing Is Being Played!**');
+            if (!player || player.queue.size === 0) return message.channel.send('❌ **Nothing Playing In This Server!**');
 
             const { channel } = message.member.voice;
             if (!channel && !message.member.roles.cache.has(role.id) && !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('**You Are Not Connected To Any Voice Channel!**');
@@ -23,10 +21,13 @@ module.exports = {
             if (!args[0]) return message.channel.send(`**The Current Volume is: \`${player.volume / 10}\`**`);
             if (isNaN(args[0])) return message.channel.send(`**Please Enter A Positive Integer!**`);
 
-            if (args[0] < 11 && args[0] > 0) player.setVolume(parseInt(args[0]) * 10);
-            else return message.channel.send('**Select Volume From 1-10**');
-
-            return message.channel.send(`**Volume Set To: \`${args[0]}\`**`);
+            if (args[0] < 11 && args[0] > 0) {
+                if (!player.playing) return message.channel.send('**Resume The Current Song To Change Volume!**');
+                else {
+                    player.setVolume(parseInt(args[0]) * 10);
+                    return message.channel.send(`**Volume Set To: \`${args[0]}\`**`);
+                };
+            } else return message.channel.send('**Select Volume From 1-10**');
         } catch (error) {
             console.error(error);
             return message.channel.send(`An Error Occurred: \`${error.message}\`!`);
