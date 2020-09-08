aee13ab860a0aacc07eb796417446081cbf6a926
Firez <55920870+FirezzzZ@users.noreply.github.com>
2020-09-08 15:21:42 +0530

    Update resume.js

--- commands/music/resume.js
+++ commands/music/resume.js
@@ -13,9 +13,7 @@ module.exports = {
             if (!role) return message.channel.send('**Role Not Found - DJ**');
 
             const player = bot.music.players.get(message.guild.id);
-            if (!player) return message.channel.send('**I Am Not Connected To Any Voice Channel!**');
-
-            if (player.queue.size === 0) return message.channel.send('**Nothing Is Being Played!**');
+            if (!player || player.queue.size === 0) return message.channel.send('❌ **Nothing Playing In This Server!**');
 
             const { channel } = message.member.voice;
             if (!channel && !message.member.roles.cache.has(role.id) && !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('**You Are Not Connected To Any Voice Channel!**');
