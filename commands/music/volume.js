4daa8d05fdfab9bb837704c38cc0f8420e5b6a14
Firez <55920870+FirezzzZ@users.noreply.github.com>
2020-09-10 21:26:41 +0530

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -7,13 +7,13 @@ module.exports = {
         usage: ', vol [volume] (1-10)',
         accessableby: "everyone"
     },
-    run: async (bot, message, args, ops) => {
+    run: async (bot, message, args) => {
         try {
             const role = message.guild.roles.cache.find(r => r.name.toUpperCase() === 'DJ');
             if (!role) return message.channel.send('**Role Not Found - DJ**');
 
             const player = bot.music.players.get(message.guild.id);
-            if (!player || player.queue.size === 0) return message.channel.send('❌ **Nothing Playing In This Server!**');
+            if (!player || player.queue.size === 0 || (player.position === 0 && !player.playing)) return message.channel.send('❌ **Nothing Playing In This Server!**');
 
             const { channel } = message.member.voice;
             if (!channel && !message.member.roles.cache.has(role.id) && !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('**You Are Not Connected To Any Voice Channel!**');
@@ -22,11 +22,8 @@ module.exports = {
             if (isNaN(args[0])) return message.channel.send(`**Please Enter A Positive Integer!**`);
 
             if (args[0] < 11 && args[0] > 0) {
-                if (!player.playing) return message.channel.send('**Resume The Current Song To Change Volume!**');
-                else {
-                    player.setVolume(parseInt(args[0]) * 10);
-                    return message.channel.send(`**Volume Set To: \`${args[0]}\`**`);
-                };
+                player.setVolume(parseInt(args[0]) * 10);
+                return message.channel.send(`**Volume Set To: \`${args[0]}\`**`);
             } else return message.channel.send('**Select Volume From 1-10**');
         } catch (error) {
             console.error(error);
