babb745e4b0cd0b609672faf6ceca7c8ff9b8628
Firez <55920870+FirezTheGreat@users.noreply.github.com>
2020-09-13 11:06:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,15 +38,15 @@ bot.on('message', async (message) => {
                 ID: message.author.id
             });
             
-            return message.channel.send(`**${message.author}, I Have Removed Your AFK Now!**`)
+            message.channel.send(`**${message.author}, I Have Removed Your AFK Now!**`)
         };
 
         if (message.content.toLowerCase() === 'hello') {
-            return message.channel.send(`Hi ${message.member}!`);
+            message.channel.send(`Hi ${message.member}!`);
         } else if (message.content.toLowerCase() === 'hi') {
-            return message.channel.send(`Hello ${message.member}!`);
+            message.channel.send(`Hello ${message.member}!`);
         } else if (message.content.toLowerCase().includes('tryout')) {
-            return message.channel.send('To join HOPE type `+apply` and fill the form in your DM.');
+            message.channel.send('To join HOPE type `+apply` and fill the form in your DM.');
         } else if (message.content.toUpperCase() === 'HOPE') {
             const embed = new MessageEmbed()
                 .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: true }))
@@ -55,21 +55,21 @@ bot.on('message', async (message) => {
                 .setImage('https://cdn.discordapp.com/attachments/724541103008776232/745967858315690085/Hope_Logo.gif')
                 .setDescription(message.guild.createdAt)
                 .setTimestamp();
-            return message.channel.send({ embed: embed });
+            message.channel.send({ embed: embed });
         } else if (message.content.toLowerCase() === 'chutiya') {
-            return message.channel.send(`${message.member} Gali mat de Madarchod`);
+            message.channel.send(`${message.member} Gali mat de Madarchod`);
         } else if (message.content.toLowerCase() === 'madarchod') {
-            return message.channel.send('Chutiye gali mat de');
+            message.channel.send('Chutiye gali mat de');
         } else if (message.content.toLowerCase() === 'chutiye') {
-            return message.channel.send(`Tu hoga chutiya ${message.member}`);
+            message.channel.send(`Tu hoga chutiya ${message.member}`);
         } else if (message.mentions.users.has('393819701924462603') && message.content === '<@!393819701924462603>') {
-            return message.channel.send('KT\'s reply is on the way.');
+            message.channel.send('KT\'s reply is on the way.');
         } else if (message.mentions.users.has('412605058086207490') && message.content === '<@!412605058086207490>') {
-            return message.channel.send(`Parsh ka net kharab hai bhai, VC nahi kar sakta`);
+            message.channel.send(`Parsh ka net kharab hai bhai, VC nahi kar sakta`);
         } else if (message.content.toLowerCase() === 'noob' && message.author.id !== '457556815345877003') {
-            return message.channel.send(`${message.member} Tu ultra pro max noob`);
+            message.channel.send(`${message.member} Tu ultra pro max noob`);
         } else if (message.content.toLowerCase().includes('inners')) {
-            return message.channel.send(`Inners Start At 9:30 p.m. Everyday!`)
+            message.channel.send(`Inners Start At 9:30 p.m. Everyday!`)
         };
     } catch (error) {
         return console.error(error);
