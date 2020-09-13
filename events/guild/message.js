9974675c0a546c015d0a8153ea2a27a15d9d8b1e
Firez <55920870+FirezTheGreat@users.noreply.github.com>
2020-09-13 11:26:15 +0530

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,4 +1,5 @@
 const { PREFIX } = require('../../config');
+const AFKList = require('../../structures/models/AFKList');
 
 module.exports = async (bot, message) => {
     try {
@@ -6,6 +7,24 @@ module.exports = async (bot, message) => {
 
         let args = message.content.slice(PREFIX.length).trim().split(/ +/g);
         let cmd = args.shift().toLowerCase();
+
+        if (message.content.includes(message.mentions.members.first())) {
+            let mentioned = await AFKList.findOne({
+                ID: message.mentions.members.first().id
+            });
+            if (mentioned) message.channel.send(`**${mentioned.name} Is Currently AFK\nReason - ${mentioned.reason}!**`);
+        };
+
+        let afkcheck = await AFKList.findOne({
+            ID: message.author.id
+        });
+        if (afkcheck) {
+            await AFKList.deleteOne({
+                ID: message.author.id
+            });
+            
+            message.channel.send(`**${message.author}, I Have Removed Your AFK Now!**`)
+        };
 
         if (!message.content.startsWith(PREFIX)) return;
 
