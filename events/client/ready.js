ee5108d54001ce096251c5901e7748113fa51904
Firez <55920870+FirezTheGreat@users.noreply.github.com>
2020-10-28 10:19:19 +0530

    Delete ready.js

--- events/client/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = bot => {
-    console.log(`${bot.user.username} is available now!`);
-    bot.user.setActivity(`${bot.users.cache.size} users!`, {
-        type: "WATCHING"
-    });
-    bot.erela.nodeConnect(bot);
-};
