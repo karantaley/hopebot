58e9597319038e10fa1d31891c023836f280cf83
Firez <55920870+FirezTheGreat@users.noreply.github.com>
2020-10-28 10:18:46 +0530

    Delete console.js

--- handler/console.js
+++
@@ -1,7 +0,0 @@
-module.exports = (bot, message) => {
-    let prompt = process.openStdin()
-    prompt.addListener("data", res => {
-        let x = res.toString().trim().split(/ +/g)
-        bot.channels.cache.get(message.guild.id).send(x.join(" "));
-    });
-};
