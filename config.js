0e5bce70b60243f5bf0101e2f8032d014f2ea03b
Firez <55920870+FirezTheGreat@users.noreply.github.com>
2020-10-28 10:18:30 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-exports.PREFIX = ''; //Example - !
-
-exports.TOKEN = ''; //discord.com/developers/applications
-
-exports.ERELA_OPTIONS = {
-    host: 'ip-address', //should match application.yml
-    port: 'port', //should match application.yml
-    password: 'password' //should match application.yml
-}; //node options for Erela.js
